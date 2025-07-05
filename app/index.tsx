249d233a26c1988d521f727d600781406caaa04a
Rafly <rafly21ardiansyah@gmail.com>
2025-07-05 11:41:06 +0700

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-5xl text-center text-blue-500 text-primary font-quicksand-semibold">
+        Welcome to my react native app
+      </Text>
+    </View>
+  );
+}
